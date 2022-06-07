6c6e581f42fbb367945d6750af57fa0742ec3e89
lucas <Lucasbriandbr@gmail.com>
2022-06-07 16:03:31 +0200

    Menu icon animation

--- web/pages/components/nav.js
+++ web/pages/components/nav.js
@@ -1,10 +1,13 @@
 import Link from "next/link"
 import styles from "../../styles/NavBar.module.css"
 import { useWindowWidth } from "../hooks/useWindowSize"
+import { useEffect, useState } from "react"
 
 export default function NavBar() {
 
     const width = useWindowWidth()
+
+    const [menuOpen, setMenuOpen] = useState(false)
 
     return(
         <>
@@ -47,10 +50,17 @@ export default function NavBar() {
                         </ul>
                     </div>
                     :
-                    <button type="button" className={`inline-flex items-center p-[15px] border-l-[1px] border-black ml-3 text-sm text-black hover:bg-white focus:outline-none dark:text-black`} aria-controls="mobile-menu" aria-expanded="false">
-                        <svg className={`w-6 h-6`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"></path></svg>
-                        <svg className={`hidden w-6 h-6`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"></path></svg>
-                    </button>
+                    // <button type="button" className={`inline-flex items-center p-[15px] border-l-[1px] border-black ml-3 text-sm text-black hover:bg-white focus:outline-none dark:text-black`} aria-controls="mobile-menu" aria-expanded="false" onClick={() => setMenuOpen(!menuOpen)}>
+                    //     <svg className={`${menuOpen?"hidden":""} w-6 h-6`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"></path></svg>
+                    //     <svg className={`${!menuOpen?"hidden":""} w-6 h-6`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"></path></svg>
+                    // </button>
+                    <div className={`${styles.menuicon}`} onClick={() => setMenuOpen(!menuOpen)}>
+                        <input className={`${styles.menuicon__cheeckbox}`} type="checkbox" />
+                        <div>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
                     }
                 </div>
             </nav>
