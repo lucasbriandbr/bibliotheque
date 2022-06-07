f12ff6c8f2950aa00d7a90220d6757c95a7f9b62
lucas <Lucasbriandbr@gmail.com>
2022-06-07 12:20:48 +0200

    Commit de ses morts

--- web/pages/components/nav.js
+++ web/pages/components/nav.js
@@ -5,10 +5,10 @@ import styles from "../../styles/NavBar.module.css"
 export default function NavBar() {
     return(
         <>
-            <nav className={`bg-white px-4 sm:px-6 py-4 dark:bg-[#faebd7] border-b-[1px] border-black`}>
-                <div className={`container flex flex-wrap justify-between items-center mx-auto`}>
+            <nav className={`bg-white dark:bg-[#faebd7] border-b-[1px] border-black`}>
+                <div className={`flex flex-wrap justify-between items-center mx-auto`}>
                 <Link href={"/"}>
-                    <a className={`flex items-center ${styles.title}`}>
+                    <a className={`flex ml-6 items-center ${styles.title}`}>
                         {/* <img src="/docs/images/logo.svg" className={`mr-3 h-6 sm:h-9`} alt="Flowbite Logo" /> */}
                         <span className={`self-center text-xl font-semibold whitespace-nowrap dark:text-black`}>Alexandria</span>
                     </a>
@@ -19,30 +19,30 @@ export default function NavBar() {
                     <svg className={`hidden w-6 h-6`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"></path></svg>
                 </button>
                 <div className={`hidden w-full md:block md:w-auto`} id="mobile-menu">
-                    <ul className={`flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium ${styles.links}`}>
+                    <ul className={`flex flex-col md:flex-row md:text-sm md:font-medium ${styles.links}`}>
                         <li>
                             <Link href={"/"}>
-                                <a className={`block py-2 pr-4 pl-3 text-black hover:bg-gray-50 md:hover:bg-transparent md:hover:text-gray-800 md:p-0 dark:text-black md:dark:hover:text-gray-800 dark:hover:text-gray-800 md:dark:hover:bg-transparent`}>Home</a>
+                                <a className={`flex text-black hover:bg-gray-50 md:hover:bg-transparent md:hover:text-gray-800 md:p-0 dark:text-black md:dark:hover:text-gray-800 dark:hover:text-gray-800 md:dark:hover:bg-transparent`}>Home</a>
                             </Link>
                         </li>
                         <li>
                             <Link href={"/"}>
-                                <a className={`block py-2 pr-4 pl-3 text-black hover:bg-gray-50 md:hover:bg-transparent md:hover:text-gray-800 md:p-0 dark:text-black md:dark:hover:text-gray-800 dark:hover:text-gray-800 md:dark:hover:bg-transparent`}>Shop</a>
+                                <a className={`flex text-black hover:bg-gray-50 md:hover:bg-transparent md:hover:text-gray-800 md:p-0 dark:text-black md:dark:hover:text-gray-800 dark:hover:text-gray-800 md:dark:hover:bg-transparent`}>Shop</a>
                             </Link>
                         </li>
                         <li>
                             <Link href={"/"}>
-                                <a className={`block py-2 pr-4 pl-3 text-black hover:bg-gray-50 md:hover:bg-transparent md:hover:text-gray-800 md:p-0 dark:text-black md:dark:hover:text-gray-800 dark:hover:text-gray-800 md:dark:hover:bg-transparent`}>Blog</a>
+                                <a className={`flex text-black hover:bg-gray-50 md:hover:bg-transparent md:hover:text-gray-800 md:p-0 dark:text-black md:dark:hover:text-gray-800 dark:hover:text-gray-800 md:dark:hover:bg-transparent`}>Blog</a>
                             </Link>
                         </li>
                         <li>
                             <Link href={"/"}>
-                                <a className={`block py-2 pr-4 pl-3 text-black hover:bg-gray-50 md:hover:bg-transparent md:hover:text-gray-800 md:p-0 dark:text-black md:dark:hover:text-gray-800 dark:hover:text-gray-800 md:dark:hover:bg-transparent`}>About us</a>
+                                <a className={`flex text-black hover:bg-gray-50 md:hover:bg-transparent md:hover:text-gray-800 md:p-0 dark:text-black md:dark:hover:text-gray-800 dark:hover:text-gray-800 md:dark:hover:bg-transparent`}>About us</a>
                             </Link>
                         </li>
                         <li>
                             <Link href={"/"}>
-                                <a className={`block py-2 pr-4 pl-3 text-black hover:bg-gray-50 md:hover:bg-transparent md:hover:text-gray-800 md:p-0 dark:text-black md:dark:hover:text-gray-800 dark:hover:text-gray-800 md:dark:hover:bg-transparent`}>Contact</a>
+                                <a className={`flex text-black hover:bg-gray-50 md:hover:bg-transparent md:hover:text-gray-800 md:p-0 dark:text-black md:dark:hover:text-gray-800 dark:hover:text-gray-800 md:dark:hover:bg-transparent`}>Contact</a>
                             </Link>
                         </li>
                     </ul>
