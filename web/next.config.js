35388476b59b1e564861d4c754f84362b4e8a5ba
lucas <Lucasbriandbr@gmail.com>
2022-06-14 15:11:03 +0200

    Desactivate experimantals

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: { images: { layoutRaw: true } }
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
