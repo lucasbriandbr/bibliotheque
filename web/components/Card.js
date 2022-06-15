9e44fd0624ee00f1cabb13eb851a7c9110444eef
lucas <Lucasbriandbr@gmail.com>
2022-06-15 13:19:51 +0200

    Update Card.js

--- web/components/Card.js
+++ web/components/Card.js
@@ -6,7 +6,7 @@ export default function Card({src, title, author, serie, tome, note, sku, }) {
     return(
         <Link href={`/book/${sku}`}>
             <a className={`flex flex-col text-black rounded-lg border-[1px] border-gray-300 ${styles.card}`}>
-                <img src={src} alt={`Une couverture de livre`} className={`rounded-lg h-full w-full`}/>
+                <img src={src} alt={`Une couverture de livre`} className={`rounded-t-lg h-full w-full`}/>
                 <div className={`flex flex-col p-4`}>
 
                     <p className={`text-sm font-semibold`}>{title}{serie===true?<span className={`text-xs font-normal`}> - Tome {tome.toString()}</span>:""}</p>
