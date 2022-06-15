20460bf9fb08cc24830e59fcdd33981067f82d25
lucas <Lucasbriandbr@gmail.com>
2022-06-15 09:42:55 +0200

    Add the rating Note in the Card

--- web/components/Card.js
+++ web/components/Card.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 import Image from "next/image"
 import styles from "../styles/Card.module.css"
 
-export default function Card({src, title, author, serie, tome, }) {
+export default function Card({src, title, author, serie, tome, note, }) {
     return(
         <Link href={`/`}>
             <a className={`flex flex-col text-black rounded-lg border-[1px] border-gray-300 ${styles.card}`}>
@@ -12,6 +12,16 @@ export default function Card({src, title, author, serie, tome, }) {
                     <p className={`text-sm font-semibold`}>{title}{serie===true?<span className={`text-xs font-normal`}> - Tome {tome.toString()}</span>:""}</p>
 
                     <p className={`text-xs italic`}>{author}</p>
+
+                    
+                    <div className={`flex items-center mt-2.5`}>
+                        <svg className={`w-5 h-5 ${note>=1?"text-yellow-400":"text-gray-400"}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                        <svg className={`w-5 h-5 ${note>=2?"text-yellow-400":"text-gray-400"}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                        <svg className={`w-5 h-5 ${note>=3?"text-yellow-400":"text-gray-400"}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                        <svg className={`w-5 h-5 ${note>=4?"text-yellow-400":"text-gray-400"}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                        <svg className={`w-5 h-5 ${note>=5?"text-yellow-400":"text-gray-400"}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                        <span className={`bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3`}>{note}</span>
+                    </div>
 
                 </div>
             </a>
--- web/constantes/Books.js
+++ web/constantes/Books.js
@@ -1,13 +1,13 @@
 export default function Books() {
     return ([
-        {title:"Akira", serie:true, tome:2, imgsrc:"/img/books/book-1.jpg", author:"Katsuhiro Otomo", },
-        {title:"Akira", serie:true, tome:6, imgsrc:"/img/books/book-2.jpeg", author:"Katsuhiro Otomo", },
-        {title:"Akira", serie:true, tome:3, imgsrc:"/img/books/book-3.jpg", author:"Katsuhiro Otomo", },
-        {title:"Akira", serie:true, tome:1, imgsrc:"/img/books/book-4.jpg", author:"Katsuhiro Otomo", },
-        {title:"Investir dans l'immobilier", serie:false, tome:1, imgsrc:"/img/books/book-5.jpeg", author:"Cédric Sernier", },
-        {title:"La brume des bois", tome:1, serie:false, imgsrc:"/img/books/book-6.webp", author:"J. Poulon", },
-        {title:"Punk 57", tome:1, serie:false, imgsrc:"/img/books/book-7.jpg", author:"Penelope Douglas", },
-        {title:"Le Hobbit", tome:1, serie:false, imgsrc:"/img/books/book-8.jpg", author:"J.R.R. Tolkien", },
-        {title:"Dark Romance", tome:1, serie:false, imgsrc:"/img/books/book-9.jpg", author:"Penelope Douglas", },
+        {title:"Akira", serie:true, tome:2, imgsrc:"/img/books/book-1.jpg", author:"Katsuhiro Otomo", note:4, },
+        {title:"Akira", serie:true, tome:6, imgsrc:"/img/books/book-2.jpeg", author:"Katsuhiro Otomo", note:5, },
+        {title:"Akira", serie:true, tome:3, imgsrc:"/img/books/book-3.jpg", author:"Katsuhiro Otomo", note:4, },
+        {title:"Akira", serie:true, tome:1, imgsrc:"/img/books/book-4.jpg", author:"Katsuhiro Otomo", note:5, },
+        {title:"Investir dans l'immobilier", serie:false, tome:1, imgsrc:"/img/books/book-5.jpeg", author:"Cédric Sernier", note:2, },
+        {title:"La brume des bois", tome:1, serie:false, imgsrc:"/img/books/book-6.webp", author:"J. Poulon", note:3, },
+        {title:"Punk 57", tome:1, serie:false, imgsrc:"/img/books/book-7.jpg", author:"Penelope Douglas", note:5, },
+        {title:"Le Hobbit", tome:1, serie:false, imgsrc:"/img/books/book-8.jpg", author:"J.R.R. Tolkien", note:5, },
+        {title:"Dark Romance", tome:1, serie:false, imgsrc:"/img/books/book-9.jpg", author:"Penelope Douglas", note:4, },
     ])
 }
