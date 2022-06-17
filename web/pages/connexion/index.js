b568f09f31f8facafc8ba242b6b521061d5ef7b3
lucas <Lucasbriandbr@gmail.com>
2022-06-17 16:43:05 +0200

    Ajout d'un début de vérification sur la page de connexion

--- web/pages/connexion/index.js
+++ web/pages/connexion/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { useState } from 'react'
+import { useRouter } from 'next/router'
 
 //  Import des objets
 
@@ -13,7 +14,30 @@ import FootLinks from "../../constantes/FootLinks"
 
 export default function Connexion() {
 
-    const [ state , setState ] = useState(true)
+    const [ state , setState ] = useState(false)
+
+    const [ mail , setMail ] = useState("")
+    const [ psswd , setPsswd ] = useState("")
+    const [ psswdConfirm , setPsswdConfirm ] = useState("")
+
+    const [ error , setError ] = useState("")
+
+    const router = useRouter()
+
+    function verifyData(email, password, passwordConfirm) {
+        
+        if (password!=passwordConfirm) {
+            setError('Les mots de passe ne correspondent pas.')
+        } else {
+            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)===false) {
+                setError('Votre aresse mail est invalide.')
+            } else {
+                setError('')
+                router.push('/')
+            }
+        }
+
+    }
 
     return(
         <>
@@ -32,23 +56,21 @@ export default function Connexion() {
                 </div>
 
                 <div className={`flex flex-col h-full w-full p-4 items-center justify-center p-[10vw]`}>
+
+                    <h3 className='block font-medium text-2xl px-5 py-2.5 mt-4'>{state?"Inscription":"Connexion"}</h3>
+                    <input type="text" id='Email' className="block w-full text-gray-900 rounded-lg border-[1px] border-black text-sm px-5 py-2.5 mt-4 focus:outline-none" placeholder='Mail' required onChange={()=>{setMail(document.getElementById('Email').value)}}/>
+                    <input type="password" id='Psswd' className="block w-full text-gray-900 rounded-lg border-[1px] border-black text-sm px-5 py-2.5 mt-4 focus:outline-none" placeholder='Password' required onChange={()=>{setPsswd(document.getElementById('Psswd').value)}}/>
                     
                     {state?
                     <>
-                        <h3 className='block font-medium text-2xl px-5 py-2.5 mb-4'>Sign Up</h3>
-                        <input type="text" className="block w-full text-gray-900 rounded-lg border-[1px] border-black text-sm px-5 py-2.5 mb-4 focus:outline-none" placeholder='Mail'/>
-                        <input type="password" className="block w-full text-gray-900 rounded-lg border-[1px] border-black text-sm px-5 py-2.5 mb-4 focus:outline-none" placeholder='Password'/>
-                        <button type="button" className="block w-full border-[1px] border-black focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 mb-4">Connect</button>
+                        <input type="password" id='PsswdConfirm' className="block w-full text-gray-900 rounded-lg border-[1px] border-black text-sm px-5 py-2.5 mt-4 focus:outline-none" placeholder='Confirm password' required onChange={()=>{setPsswdConfirm(document.getElementById('PsswdConfirm').value)}}/>
+                        <button type="button" className="block w-full border-[1px] border-black focus:outline-none mt-4 font-medium rounded-lg text-sm px-5 py-2.5 transition-all ease-in-out duration-300 hover:bg-gray-100" onClick={()=>{verifyData(mail, psswd, psswdConfirm)}}>S'enregistrer</button>
+                        <p className={`text-xs w-full text-red-600 font-medium mt-1`}>{error}</p>
                     </>
                     :
-                    <>
-                        <h3 className='block font-medium text-2xl px-5 py-2.5 mb-4'>Sign In</h3>
-                        <input type="text" className="block w-full text-gray-900 rounded-lg border-[1px] border-black text-sm px-5 py-2.5 mb-4 focus:outline-none" placeholder='Mail'/>
-                        <input type="password" className="block w-full text-gray-900 rounded-lg border-[1px] border-black text-sm px-5 py-2.5 mb-4 focus:outline-none" placeholder='Password'/>
-                        <button type="button" className="block w-full border-[1px] border-black focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 mb-4">Connect</button>
-                    </>
+                    <button type="button" className="block w-full border-[1px] border-black focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 mt-4 transition-all ease-in-out duration-300 hover:bg-gray-100">Se connecter</button>
                     }
-                    <p className='flex flex-col text-center font-normal text-xs px-5 py-2.5 mb-4'>{state?"Pas encore inscrit ?":"Déjà inscrit ?"}<button type="button" className='block font-medium text-xs mt-2 underline' onClick={()=>{setState(!state)}}>{state?"Sign In":"Sign Up"}</button></p>
+                    <p className='flex flex-col text-center font-normal text-xs px-5 py-2.5 mt-4'>{state?"Déjà inscrit ?":"Pas encore inscrit ?"}<button type="button" className='block font-medium text-xs mt-2 underline' onClick={()=>{setState(!state)}}>{state?"Connexion":"Inscription"}</button></p>
 
                 </div>
 
