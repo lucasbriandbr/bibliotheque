28f1b36649f83e80d93edd6e3bf00cb8baa6ea7c
lucas <Lucasbriandbr@gmail.com>
2022-06-21 13:13:04 +0200

    Poop

--- web/components/BigCard.js
+++ web/components/BigCard.js
@@ -5,13 +5,13 @@ import styles from "../styles/Card.module.css"
 export default function BigCard({src, title, author, serie, tome, note, sku, }) {
     return(
         <Link href={`/book/${sku}`}>
-            <a className={`relative flex items-end text-black bg-white min-h-[50vh] min-w-[45vw]`}>
+            <a className={`relative flex items-end text-black bg-white min-h-[50vh] min-w-[35vw]`}>
+
+                <img src={src} alt={`Une couverture de livre`} className={`flex absolute bottom-8 left-8 self-start justify-self-start h-4/5 w-auto border border-black rounded-lg`}/>
 
                 <div className="static flex bg-blue-300 w-full rounded-lg h-2/3">
 
                 </div>
-
-                <img src={src} alt={`Une couverture de livre`} className={`flex absolute bottom-8 left-8 self-start justify-self-start h-4/5 w-auto border border-black rounded-lg`}/>
 
             </a>
         </Link>
--- web/pages/index.js
+++ web/pages/index.js
@@ -29,6 +29,10 @@ export default function Home() {
 
       </div>
 
+      <div className='flex flex-row pt-8 pr-8 pl-8 w-full items-center justify-center'>
+        <h1>Nouveautés</h1>
+      </div>
+
       <div className={`flex flex-row bg-white text-white p-8 gap-8 overflow-x-scroll overflow-y-hidden`}>
         
         <BigCard src={Books()[0].imgsrc} title={Books()[0].title} author={Books()[0].author} serie={Books()[0].serie} tome={Books()[0].tome} note={Books()[0].note} sku={Books()[0].sku}/>
@@ -36,6 +40,10 @@ export default function Home() {
         <BigCard src={Books()[1].imgsrc} title={Books()[1].title} author={Books()[1].author} serie={Books()[1].serie} tome={Books()[1].tome} note={Books()[1].note} sku={Books()[1].sku}/>
         
         <BigCard src={Books()[2].imgsrc} title={Books()[2].title} author={Books()[2].author} serie={Books()[2].serie} tome={Books()[2].tome} note={Books()[2].note} sku={Books()[2].sku}/>
+        
+        <BigCard src={Books()[3].imgsrc} title={Books()[3].title} author={Books()[3].author} serie={Books()[3].serie} tome={Books()[3].tome} note={Books()[3].note} sku={Books()[3].sku}/>
+        
+        <BigCard src={Books()[4].imgsrc} title={Books()[4].title} author={Books()[4].author} serie={Books()[4].serie} tome={Books()[4].tome} note={Books()[4].note} sku={Books()[4].sku}/>
 
       </div>
 
