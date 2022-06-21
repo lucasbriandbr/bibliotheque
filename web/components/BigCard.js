85def89647f9c7b5d7b53f8b404f293ae5879c77
lucas <Lucasbriandbr@gmail.com>
2022-06-21 12:07:17 +0200

    Update BigCard.js

--- web/components/BigCard.js
+++ web/components/BigCard.js
@@ -7,11 +7,11 @@ export default function BigCard({src, title, author, serie, tome, note, sku, }) 
         <Link href={`/book/${sku}`}>
             <a className={`relative flex items-end text-black bg-white min-h-[50vh] min-w-[45vw]`}>
 
-                <div className="static bg-blue-300 w-full rounded-lg h-2/3">
+                <div className="static flex bg-blue-300 w-full rounded-lg h-2/3">
 
                 </div>
 
-                <img src={src} alt={`Une couverture de livre`} className={`absolute bottom-8 left-8 self-start justify-self-start h-auto w-2/5 border border-black rounded-lg`}/>
+                <img src={src} alt={`Une couverture de livre`} className={`flex absolute bottom-8 left-8 self-start justify-self-start h-4/5 w-auto border border-black rounded-lg`}/>
 
             </a>
         </Link>
