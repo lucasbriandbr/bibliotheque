283437c46a8deef4b1d8c9829d44179903317976
lucas <Lucasbriandbr@gmail.com>
2022-06-22 16:07:26 +0200

    ça va être rigolo tiens donc vindieu

---
+++ web/bdd/connecteur.js
@@ -0,0 +1,6 @@
+import { PrismaClient } from '@prisma/client'
+
+export default function connectBdd() {
+    const prisma = new PrismaClient()
+    return prisma
+}
--- web/constantes/NavLinks.js
+++ web/constantes/NavLinks.js
@@ -1,7 +1,7 @@
 export default function NavLinks() {
     return ([
         { title:"Home", link:"/", path:`` },
-        { title:"Shop", link:"/shop", path:`` },
+        // { title:"Shop", link:"/shop", path:`` },
         { title:"Search", link:"/search", path:`` },
         { title:"Blog", link:"/blog", path:`` },
         { title:"Connexion", link:"/connexion", path:`` },
