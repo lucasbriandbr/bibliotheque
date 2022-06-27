420f9e0b8b99b9ef3652c79de15881101cc4f4bb
lucas <Lucasbriandbr@gmail.com>
2022-06-27 09:51:56 +0200

    On commit et alleeeez

--- web/pages/api/v2/post/postUsers.js
+++
@@ -1,7 +0,0 @@
-import getUsers from "../../../../bdd/post/users"
-
-export default async (req, res) => {
-    const {number} = req.query
-    const users = await getUsers(number)
-    return res.status(200).json({users})
-}
